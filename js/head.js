6b6c684b97cedf0eb28fe1df3daf0d2358a32678
ririne19 <marinehuard19@icloud.com>
2026-06-22 12:36:17 +0200

    changement hiérarchie index.html

--- js/head.js
+++ js/head.js
@@ -1,11 +1,14 @@
+// Détecte si la page courante est dans un sous-dossier (ex: Pages/)
+const base = window.location.pathname.includes('/Pages/') ? '../' : '';
+
 // Favicon
 document.head.insertAdjacentHTML('beforeend', `
-  <link rel="icon" type="image/svg+xml" href="../Divers/favicon.png">
+  <link rel="icon" type="image/svg+xml" href="${base}Divers/favicon.png">
 `);
 
 // Header CSS
 document.head.insertAdjacentHTML('beforeend', `
-  <link rel="stylesheet" href="../Styles/header.css">
+  <link rel="stylesheet" href="${base}Styles/header.css">
 `);
 
 // Header
@@ -14,7 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
   if (header) {
     header.innerHTML = `
       <div class="header-top">
-        <div class="logo"><a href="../Pages/index.html"><img src="../Divers/logoIAVelay.svg" alt="Logo"></a></div>
+        <div class="logo"><a href="${base}index.html"><img src="${base}Divers/logoIAVelay.svg" alt="Logo"></a></div>
         <div class="search-bar">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
